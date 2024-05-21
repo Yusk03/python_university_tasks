58bddc8559cb90b2a9b8e36859dd03dca790545c
Yusk <psixopat1998@gmail.com>
2024-05-21 21:01:24 +0300

    solved tasks 1-11

---
+++ task_03.py
@@ -0,0 +1,26 @@
+"""
+Якщо запустити код завдання, на екран буде виведено:
+$ python task_03.py
+Python is a high-level, interpreted, general-purpose programming language.
+
+Отримати з рядка start_data такий список (видалені коми та точка і рядок
+поділено на слова):
+['Python', 'is', 'a', 'high-level', 'interpreted', 'general-purpose', 'programming', 'language']
+
+За допомогою print вивести отриманий список на екран:
+$ python task_03.py
+['Python', 'is', 'a', 'high-level', 'interpreted', 'general-purpose', 'programming', 'language']
+
+Тут дуже важливий момент, що треба отримати саме список (тип даних), а не,
+наприклад, рядок, який схожий на зображений список.
+
+Обмеження: рядок start_data не можна змінювати вручну, тільки за допомогою Python.
+"""
+import re
+
+start_data = "Python is a high-level, interpreted, general-purpose programming language."
+
+start_data = re.findall(r'[a-zA-Z\-]+', start_data)
+
+print(start_data)
+
